0e3fd26e30a7f1c2d5d78369850586ee6dad055a
cilegann <s571991@gmail.com>
2019-05-23 14:38:37 +0800

    init

--- fetcher.py
+++ fetcher.py
@@ -1,28 +1,41 @@
-class company():
-    def __init__():
-        pass
+import configparser
+class fetcher():
+    def __init__(self,config):
+        cfp=configparser.ConfigParser()
+        cfp.read(config)
+        self.dbDB=cfp.get('db','db')
+        self.dbHost=cfp.get('db','host')
+        self.dbPort=cfp.get('db','port')
+        self.dbUser=cfp.get('db','user')
+        self.dbPwd=cfp.get('db','pwd')
+
+class company(fetcher):
+    def __init__(self,config='./config.ini'):
+        super().__init__(config)
     def update():
         pass
+    def dev(self):
+        print(self.dbPwd)
 
-class daily():
-    def __init__():
-        pass
+class daily(fetcher):
+    def __init__(self,config='./config.ini'):
+        super().__init__(config)
     def update():
         pass
     def getHistory():
         pass
 
 class weekly():
-    def __init__():
-        pass
+    def __init__(self,config='./config.ini'):
+        super().__init__(config)
     def update():
         pass
     def getHistory():
         pass
 
 class holdclass():
-    def __init__():
-        pass
+    def __init__(self,config='./config.ini'):
+        super().__init__(config)
     def update():
         pass
     def getHistory():
